6eed0af1c63525468d3a908abed5dddf32886c96
Olamide Anifowoshe <anifowosheolamidee@gmail.com>
2022-09-01 00:25:04 +0100

    add 404 page

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.use(shop);
 //   // next()
 // });
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found!</h1>");
+});
+
 // const server = http.createServer(app);
 
 app.listen(8000);
