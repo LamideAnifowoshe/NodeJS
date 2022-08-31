971240f46bee96c19b2307c667a4659f803afb36
Olamide Anifowoshe <anifowosheolamidee@gmail.com>
2022-08-31 19:40:31 +0100

    get response from server using express

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in the middleware!");
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("in another middleware!");
+app.use("/users", (req, res, next) => {
+  console.log("in another middleware handling /users path!");
   res.send("<h1>Hello from HTML!</h1>");
   // next()
+});
+
+app.use("/", (req, res, next) => {
+  console.log("/ middleware");
+  //   next();
+  res.send("<p>in the middleware handling just / path!</p>");
 });
 
 // const server = http.createServer(app);
