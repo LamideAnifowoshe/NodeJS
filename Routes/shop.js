ee64c614edf0d88e07a9e5bff7568f236f737893
Olamide Anifowoshe <anifowosheolamidee@gmail.com>
2022-09-01 16:26:52 +0100

    use the path module

--- Routes/shop.js
+++ Routes/shop.js
@@ -1,10 +1,11 @@
+const path = require("path");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   console.log("/ middleware");
   //   next();
-  res.send("<p>in the middleware handling just / path!</p>");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
