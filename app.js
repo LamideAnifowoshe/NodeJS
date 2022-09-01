463643f07f269cd04a5ae45beb6a6a5adf1bd7cb
Olamide Anifowoshe <anifowosheolamidee@gmail.com>
2022-09-01 21:40:31 +0100

    add util folder for root directoryt

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 // const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -18,7 +19,7 @@ app.use(shop);
 // });
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found!</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 // const server = http.createServer(app);
