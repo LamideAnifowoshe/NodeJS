67057f32b500421d63ec76999bdd51e1747312fa
Olamide Anifowoshe <anifowosheolamidee@gmail.com>
2022-09-05 14:01:45 +0100

    trial

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(shop);
 //   res.send("<h1>Hello from HTML!</h1>");
 //   // next()
 // });
-
+// trial
 app.use((req, res, next) => {
   res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
