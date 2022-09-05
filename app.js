bc240908a022784983b5df8764618dc37b2a912a
Olamide Anifowoshe <anifowosheolamidee@gmail.com>
2022-09-05 15:15:55 +0100

    random commit

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(shop);
 //   res.send("<h1>Hello from HTML!</h1>");
 //   // next()
 // });
+//
 
 app.use((req, res, next) => {
   res.status(404).render("404", { pageTitle: "Page Not Found" });
