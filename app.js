11bf5a5da3231364abbf3afad18c07b8ad733192
Olamide Anifowoshe <anifowosheolamidee@gmail.com>
2022-09-05 20:23:00 +0100

    switch template engine to handlebars

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const admin = require("./routes/admin");
 const shop = require("./routes/shop");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.engine("handlebars", expressHbs());
+app.set("view engine", "handlebars");
+
+// app.set("view engine", "pug");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
