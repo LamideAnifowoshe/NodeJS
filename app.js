3ed1129beb53f7559654bdd03047aa53627df32f
Olamide Anifowoshe <anifowosheolamidee@gmail.com>
2022-09-05 21:35:05 +0100

    switching to ejs template engine

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const admin = require("./routes/admin");
 const shop = require("./routes/shop");
-const expressHbs = require("express-handlebars");
+// const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine("handlebars", expressHbs());
-app.set("view engine", "handlebars");
+// app.engine("handlebars", expressHbs());
+// app.set("view engine", "handlebars");
 
 // app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
