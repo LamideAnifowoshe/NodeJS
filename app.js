14280a130bcddee179ccb8687a75c57dc46a4c5d
Olamide Anifowoshe <anifowosheolamidee@gmail.com>
2022-09-12 23:57:56 +0100

    add a mysql database to app

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const admin = require("./routes/admin");
 const shop = require("./routes/shop");
 const errorController = require("./controllers/error");
 // const expressHbs = require("express-handlebars");
+const db = require("./util/database");
 
 const app = express();
 
@@ -15,6 +16,8 @@ const app = express();
 // app.set("view engine", "pug");
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+db.execute("SELECT * from PRODUCTS").then().catch();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
