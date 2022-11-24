f7f99a132277c8c2d07e01236a3160d34cfa9639
Olamide Anifowoshe <anifowosheolamidee@gmail.com>
2022-11-24 14:27:06 +0100

    Add a user model

--- models/user.js
+++ models/user.js
@@ -1,9 +1,34 @@
-class User {
-  constructor(username, email) {
-    this.name = username;
-    this.email = email;
-  }
-}
+const mongoose = require("mongoose");
+const { schema } = require("./product");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: Number,
+    required: true,
+  },
+  cart: {
+    items: [
+      {
+        productId: { type: Schema.Types.ObjectId, required: true },
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
+
+// class User {
+//   constructor(username, email) {
+//     this.name = username;
+//     this.email = email;
+//   }
+// }
 
 // const sequelize = require("../util/database");
 
